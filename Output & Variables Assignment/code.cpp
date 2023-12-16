2c1a55b2b42f42ebe2bd2533af0f661a0d3df798
topaja <saha.topaja@gmail.com>
2023-12-16 13:04:21 +0530

    intro to c++ assign1

---
+++ Output & Variables Assignment/code.cpp
@@ -0,0 +1,58 @@
+// 1
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Physics\nWallah";
+    return 0;
+}
+
+
+// 2
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<2*5;
+    return 0;
+}
+
+
+// 3
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Size of int : "<<sizeof(int)<<endl;
+    cout<<"Size of bool : "<<sizeof(bool)<<endl;
+    cout<<"Size of float : "<<sizeof(float);
+    return 0;
+}
+
+
+// 4
+output is : 6 4
+
+
+// 5
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int r=10;
+    float circum = 2* 3.14 * r;
+    cout<<"Circumference is : "<<circum;
+    return 0;
+}
+
+
+// 6
+ 01PWskills - cannot be a variable name because starting with number
+ _FLOAT - can be a variable name
+ int - cannot be a variable name because it is a keyword
+ FLOAT - can be a variable name
+ You will succeed - cannot be a variable name because blank space not allowed in variable name 
